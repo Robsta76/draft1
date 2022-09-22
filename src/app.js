44d57f5c2909ba1da28160ce48203290234357b2
RobFernandes <robfernandes76@gmail.com>
2022-09-22 18:49:05 +0100

    my first draft

--- src/app.js
+++ src/app.js
@@ -10,22 +10,22 @@ function toggleButton() {
 hamburgerButton.addEventListener('click', toggleButton)
 
 // hand wave
-.wave {
-  animation-name: wave-animation;
-  animation-duration: 2.5s;
-  animation-iteration-count: infinite;
-  transform-origin: 70% 70%;
-  display: inline-block;
-}
+// .wave {
+//   animation-name: wave-animation;
+//   animation-duration: 2.5s;
+//   animation-iteration-count: infinite;
+//   transform-origin: 70% 70%;
+//   display: inline-block;
+// }
 
-@keyframes wave-animation {
-  0% { transform: rotate( 0.0deg) }
- 10% { transform: rotate(14.0deg) }
- 20% { transform: rotate(-8.0deg) }
- 30% { transform: rotate(14.0deg) }
- 40% { transform: rotate(-4.0deg) }
- 50% { transform: rotate(10.0deg) }
- 60% { transform: rotate( 0.0deg) }
+// @keyframes wave-animation {
+//   0% { transform: rotate( 0.0deg) }
+//  10% { transform: rotate(14.0deg) }
+//  20% { transform: rotate(-8.0deg) }
+//  30% { transform: rotate(14.0deg) }
+//  40% { transform: rotate(-4.0deg) }
+//  50% { transform: rotate(10.0deg) }
+//  60% { transform: rotate( 0.0deg) }
 
 // contact form
 
